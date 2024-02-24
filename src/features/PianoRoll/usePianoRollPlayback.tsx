7da8fd605d345352f380b704a01a44a238ec8cc6
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-02-23 17:27:45 -0800

    :construction: track sustained keys

--- src/features/PianoRoll/usePianoRollPlayback.tsx
+++ src/features/PianoRoll/usePianoRollPlayback.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { usePianoRollStore } from "../../store/piano-roll";
 import * as Tone from "tone";
+import { Note } from "../../store/input";
 
 const usePianoRollPlayback = () => {
   const { notes, playing, startTime } = usePianoRollStore();
@@ -20,10 +21,18 @@ const usePianoRollPlayback = () => {
       currentTime.current - startTime
     );
     const elapsedTime = currentTime.current - startTime;
-    const notesToPlay = allNoteTimes.filter((rawTime) => {
+    const notesToPlay = allNoteTimes.reduce((combineNotes, rawTime) => {
       const time = Number(rawTime);
-      return time <= elapsedTime + 0.1 && time >= elapsedTime - 0.1;
-    });
+      const noteData = notes[time];
+      const totalTime = time + noteData.length;
+      const isBefore = time <= elapsedTime + 0.1;
+      const isAfter = time >= elapsedTime - 0.1;
+      const isDuring = elapsedTime > time && elapsedTime < totalTime;
+      if ((isBefore && isAfter) || isDuring) {
+        combineNotes[noteData.note] = true;
+      }
+      return combineNotes;
+    }, {} as Record<Note, boolean>);
     console.log("notes to play", notesToPlay);
 
     animationRef.current = requestAnimationFrame(tickClock);
